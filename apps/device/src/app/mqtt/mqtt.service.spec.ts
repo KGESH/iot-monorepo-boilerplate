98c2a13b462acf9b47d156cfcdd2b9136d671201
KGESH <seonghyeon.jee@gmail.com>
2022-08-13 22:55:36 +0900

    Test: Mqtt broker test

---
+++ apps/device/src/app/mqtt/mqtt.service.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from '@nestjs/testing';
+import { MqttService } from './mqtt.service';
+import { MQTT_TOKEN } from './enum';
+import { Observable } from 'rxjs';
+
+const mockBroker = {
+  emit: (topic: string, payload: unknown): Observable<unknown> => {
+    return new Observable<unknown>();
+  },
+  send: (topic: string, payload: unknown): Observable<unknown> => {
+    return new Observable<unknown>();
+  },
+};
+
+describe('MQTT Service', () => {
+  let mqttService: MqttService;
+
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      providers: [
+        MqttService,
+        { provide: MQTT_TOKEN.DEVICE_BROKER, useValue: mockBroker },
+      ],
+    }).compile();
+
+    mqttService = app.get<MqttService>(MqttService);
+  });
+
+  describe('MqttService', () => {
+    it('should be define', () => {
+      console.log(mqttService);
+      expect(mqttService).toBeDefined();
+    });
+
+    it('should be publish message', async () => {
+      const publishResult = await mqttService.publish('topic', 'payload');
+      expect(publishResult).toBeUndefined();
+    });
+  });
+});
