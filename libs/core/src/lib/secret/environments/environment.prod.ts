4dc5d7b419c5b3884094de149942ba04658bd1c7
KGESH <seonghyeon.jee@gmail.com>
2022-08-14 21:01:19 +0900

    Feat: environment service

---
+++ libs/core/src/lib/secret/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  API_GATEWAY_URL: 'gateway',
+  API_GATEWAY_PORT: 7777,
+
+  DEVICE_MS_URL: 'device',
+  DEVICE_MS_PORT: 8000,
+
+  DATABASE_TYPE: 'postgres',
+  DATABASE_HOST: 'db',
+  DATABASE_NAME: 'test',
+  DATABASE_USER: 'postgres',
+  DATABASE_PASSWORD: 'test',
+  DATABASE_PORT: 5432,
+
+  REDIS_HOST: 'redis',
+  REDIS_PORT: 6379,
+
+  MQTT_BROKER_URL: 'mosquitto',
+};
---
+++ libs/core/src/lib/secret/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: false,
+  API_GATEWAY_URL: '0.0.0.0',
+  API_GATEWAY_PORT: 7777,
+
+  DEVICE_MS_URL: '0.0.0.0',
+  DEVICE_MS_PORT: 8000,
+
+  DATABASE_TYPE: 'postgres',
+  DATABASE_HOST: '0.0.0.0',
+  DATABASE_NAME: 'test',
+  DATABASE_USER: 'postgres',
+  DATABASE_PASSWORD: 'test',
+  DATABASE_PORT: 5432,
+
+  REDIS_HOST: '0.0.0.0',
+  REDIS_PORT: 6379,
+
+  MQTT_BROKER_URL: 'mqtt://0.0.0.0:1883',
+};
