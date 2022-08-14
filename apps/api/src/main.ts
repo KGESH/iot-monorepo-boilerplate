90f448fd24ceb1350169362732869debf3c8c306
KGESH <seonghyeon.jee@gmail.com>
2022-08-14 20:54:51 +0900

    Refactor: cleanup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,25 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 import { ISecretService } from '@iot-boilerplate/core';
 import { buildSwagger } from './utils/swagger/builder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const secretService = app.get(ISecretService);
 
-  const port = secretService.API_GATEWAY_PORT || process.env.PORT || 3333;
-  const url = secretService.API_GATEWAY_URL;
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = secretService.API_GATEWAY_PORT || 3333;
+  const url = secretService.API_GATEWAY_URL || 'localhost';
+
   buildSwagger(app);
   await app.listen(port);
+  console.log(`api process env list: `, { ...process.env });
 
   Logger.log(
     `🚀 Application is running on: http://${url}:${port}/${globalPrefix}`
--- apps/device/src/main.ts
+++ apps/device/src/main.ts
@@ -1,22 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 import { ISecretService } from '@iot-boilerplate/core';
+import { environment } from '@iot-boilerplate/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'device';
+
   app.setGlobalPrefix(globalPrefix);
   const secretService = app.get(ISecretService);
-  const port = secretService.DEVICE_MS_PORT || process.env.PORT || 33333;
-  const url = secretService.DEVICE_MS_URL;
+  const port = secretService.DEVICE_MS_PORT || 4444;
+  const url = secretService.DEVICE_MS_URL || 'localhost';
   await app.listen(port);
+
+  console.log(`device dir envs:`, { ...environment });
+
   Logger.log(
     `🚀 Application is ${process.env.NODE_ENV}running ${url} on: http://localhost:${port}/${globalPrefix}`
   );
