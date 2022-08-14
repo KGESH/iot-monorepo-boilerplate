1e0f8a26418166b44232e65548d6dee000410838
KGESH <seonghyeon.jee@gmail.com>
2022-08-14 20:51:53 +0900

    Feat: create util

---
+++ libs/util/src/lib/topics/api-topic.util.ts
@@ -0,0 +1,34 @@
+/** Todo: Refactoring topics */
+export enum ESlaveConfigTopic {
+  ALL = 'config',
+  THERMOMETER = 'config/temperature',
+  WATER_PUMP = 'config/water',
+  LED = 'config/led',
+}
+
+export enum ESlaveTurnPowerTopic {
+  WATER_PUMP = 'power/water',
+  LED = 'power/led',
+  FAN = 'power/fan',
+}
+
+export enum EPowerState {
+  ON = 'on',
+  OFF = 'off',
+}
+
+export enum ESensor {
+  LED = 'led',
+  WATER_PUMP = 'water',
+}
+
+export enum ESlaveState {
+  ALL = 'state',
+  THERMOMETER = 'temperature/state',
+  WATER_PUMP = 'water/state',
+  LED = 'led/state',
+}
+
+export const POLLING = 'master/+/polling';
+
+export const TEMPERATURE_WEEK = 'temperature/week';
